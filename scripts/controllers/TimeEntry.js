f9c38e52c984247503fb718fd609827675b84a5c
macfisher <macfisher01@gmail.com>
2015-05-25 17:34:14 -0600

    added logNewTime method to TimeEntry controller

--- scripts/controllers/TimeEntry.js
+++ scripts/controllers/TimeEntry.js
@@ -34,5 +34,39 @@
 			}
 			
 		}
+		
+		
+		// submits time entry via ng-click(Log Time)
+		vm.logNewTime = function() {
+			
+			// check that clock-in time isn't after clock-out time
+			if (vm.clockOut < vm.clockIn) {
+				alert("You can't clock out before you clock-in!");
+				return;
+			}
+			
+			// check if time entry is > 0
+			if (vm.clockOut - vm.clockIn === 0) {
+				alert("Your time entry has to be greater than zero!");
+				return;
+			}
+			
+			vm.timeentries.push(
+				{
+					"user_id":1,
+					"user_firstname":"TestFirstName",
+					"user_lastname":"TestLastName",
+					"start_time":vm.clockIn,
+					"end_time":vm.clockOut,
+					"loggedTime": time.getTimeDiff(vm.clockIn, vm.clockOut),
+					"comment":vm.comment
+				}
+			);
+			
+			updateTotalTime(vm.timeentries);
+			
+			vm.comment = "";
+			
+		}
 	
 })();
