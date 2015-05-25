7d3dc247a6605b24559ca6ac40249b5241a9b50d
macfisher <macfisher01@gmail.com>
2015-05-25 16:45:20 -0600

    added methods to time.js service

--- scripts/services/time.js
+++ scripts/services/time.js
@@ -26,9 +26,45 @@
 				
 			}
 			
+			
+			// use Moment.js to get the duration of the time entry
+			function getTimeDiff(start, end) {
+				
+				var diff = moment(end).diff(moment(start));
+				var duration = moment.duration(diff);
+				
+				return {
+					duration: duration
+				}
+				
+			}
+			
+			
+			// add up the total time for all time entries
+			function getTotalTime(timeentries) {
+				
+				var totalMilliseconds = 0;
+				
+				angular.forEach(timeentries, function(key) {
+					totalMilliseconds += key.loggedTime.duration._milliseconds;
+				});
+				
+				// after 24hrs, the Moment.js duration obj. reports the next
+				// unit up, which is days.
+				// using the asHours method and rounding down with Math.floor
+				// gives us the total hour, instead.
+				return {
+					hours: Math.floor(moment.duration(totalMilliseconds).asHours()),
+					minutes: moment.duration(totalMilliseconds).minutes()
+				}
+				
+			}
+			
 			return {
-				getTime: getTime
-			};
+				getTime: getTime,
+				getTimeDiff: getTimeDiff,
+				getTotalTime: getTotalTime
+			}
 			
 		}
 	
